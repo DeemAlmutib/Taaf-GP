a473a8628e4a5bf635ac5c5d6fa7be13ab5e1b4e
DeemAlmutib <105022105+DeemAlmutib@users.noreply.github.com>
2022-12-09 23:49:13 +0300

    Initial commit

---
+++ taaf/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+// This widget is the root
+// of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase',
+      home: AddData(),
+    );
+  }
+}
+
+class AddData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("geeksforgeeks"),
+      ),
+      body: Center(
+        child: FloatingActionButton(
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add),
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('data')
+                .add({'text': 'data added through app'});
+          },
+        ),
+      ),
+    );
+  }
+}
