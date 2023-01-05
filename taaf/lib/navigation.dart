7a3b29e226419bf39bf93ef24eb08142c059cc08
Saly Ghorab <saly20012015@gmail.com>
2023-01-05 18:23:36 +0300

    fixed navigation font and direction

--- taaf/lib/navigation.dart
+++ taaf/lib/navigation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:taaf/homepage.dart';
 import 'package:taaf/report.dart';
@@ -15,13 +16,13 @@ class Navigation extends StatefulWidget {
 
 class _NavigationState extends State<Navigation> {
    final pages = [
-    const HompageWidget(),
     ProfilePage(),
     const HistoryWidget(),
+    const HompageWidget(),
     
   ];
 
-  int Index = 0;
+  int Index = 2;
   
   @override
   Widget build(BuildContext context) {
@@ -33,6 +34,7 @@ class _NavigationState extends State<Navigation> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5),
           child: GNav(
+            selectedIndex: Index,
             backgroundColor: Color(0xFF007282),
             color: Colors.white,
             activeColor: Colors.white,
@@ -41,22 +43,57 @@ class _NavigationState extends State<Navigation> {
             gap: 8,
             onTabChange: (Index){
               switch (Index){
-                case 0 :
+                case 2 :
                 HompageWidget();
+              break ; 
+              
+                case 1 :
+               HistoryWidget();
+               break ; 
+
+               case 0: 
+               ProfilePage(); 
+               break; 
+
               }
              
+                
+              
+             
 
-
+            }
               
               
-            } ,
+             ,
             
             
             tabs: [
+              
+
              
                 GButton(
                 icon: Icons.person,
                 text: 'الملف الشخصي',
+                textStyle: GoogleFonts.tajawal(
+                  color: Colors.white
+                 ),
+                 onPressed: () {
+                setState(() {
+                  Index = 0;
+                });
+              },
+                ),
+                 GButton(
+                icon: Icons.history,
+                text:  'التشخيصات السابقة',
+                 textStyle: GoogleFonts.tajawal(
+                  color: Colors.white
+                 ),
+                
+                 
+               
+                 // GoogleFonts.tajawal() ,
+               
                  onPressed: () {
                 setState(() {
                   Index = 1;
@@ -64,20 +101,15 @@ class _NavigationState extends State<Navigation> {
               },
                 ),
                  GButton(
-                icon: Icons.history,
-                text:  'التشخيصات السابقة',
+                icon: Icons.home,
+                text: 'الرئيسية',
+              textStyle: GoogleFonts.tajawal(
+                  color: Colors.white
+                 ),
+                
                  onPressed: () {
                 setState(() {
                   Index = 2;
-                });
-              },
-                ),
-                 GButton(
-                icon: Icons.home,
-                text: 'الرئيسية',
-                 onPressed: () {
-                setState(() {
-                  Index = 0;
                 });
               },
                 ),
