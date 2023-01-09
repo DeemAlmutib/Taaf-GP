34e84eee0dcdb9ee45b057fc3adbf461315b8b75
Sarah Alarifi <sara.ibrahim.arifi1@gmail.com>
2023-01-09 04:09:15 +0300

    Update questions + translate symptoms

---
+++ taaf/lib/Translation/findArabicSymptom.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show PlatformException, rootBundle;
+import 'Symptoms.dart';
+
+class findArabicSymptom  {
+
+   
+  
+  List<Symptoms> symptomsList = [];
+  List<Symptoms> foundSymptom = [];
+
+  
+   Future<List<Symptoms>> ReadJsonData() async {
+    //read json file
+    final jsondata = await rootBundle.loadString('lib/Translation/SymptomsData.json');
+    //decode json data as list
+    final list = json.decode(jsondata) as List<dynamic>;
+
+    //map json and initialize using DataModel
+    return list.map((e) => Symptoms.fromJson(e)).toList();
+  }
+
+   Future<void> findSymptom(String name) async {
+    //foundSymptom.clear();
+    symptomsList = await ReadJsonData() as List<Symptoms>;
+
+   //  String arabicSymp = " ";
+   
+    for (var i = 0; i < symptomsList.length; i++) {
+      
+     //foundSymptom.clear();
+
+      if (name == symptomsList[i].nameEn) {
+         foundSymptom.clear();
+        print(symptomsList[i].nameAr);
+        foundSymptom.add(Symptoms.fromJson(symptomsList[i].toJson()));
+        print("found: ");
+         print(foundSymptom[0].nameAr);
+        
+       // arabicSymp = symptomsList[i].nameAr!;
+        break;
+      } 
+    }
+
+    // return arabicSymp;
+    
+  }
+}
